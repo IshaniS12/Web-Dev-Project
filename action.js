06c44a7c6c1cbf756295f9e9579eca8f8f75b286
IshaniS12 <99123077+IshaniS12@users.noreply.github.com>
2022-02-06 06:33:13 -0500

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeToRed() {
+    document.body.style.backgroundColor = "Red";
+  }
+  
+  function changeToOrange() {
+    document.body.style.backgroundColor = "Orange";
+  }
+  
+  function changeToGreen() {
+    document.body.style.backgroundColor = "Green";
+  }
+  
+  function changeToBlue() {
+    document.body.style.backgroundColor = "blue";
+  }
